68e19901c2b86f566c566863d2bcf84511d8a948
V <veronika.m.winters@gmail.com>
2023-05-05 08:00:18 +0000

    update files.ts

--- src/lib/files.ts
+++ src/lib/files.ts
@@ -29,6 +29,13 @@ const groupBy = <T>(array: T[], predicate: (value: T, index: number, array: T[])
 		return acc;
 	}, {} as { [key: string]: T[] });
 
+/**
+* Returns an object with keys as dates in the format of "YYYY-MM-DD"
+* and values as an array of Subnode objects with that date as their node property.
+* @param subnodes Array of Subnode objects to group by date.
+* @returns Object with keys as dates and values as arrays of Subnode objects.
+*/
+
 export const journals = (subnodes: Subnode[]) => {
 	let nodes = []
 
@@ -40,17 +47,34 @@ export const journals = (subnodes: Subnode[]) => {
 	return s
 }
 
+/**
+ * Filters a list of Subnode objects based on their node property.
+ *
+ * @param {Subnode[]} subnodes - The list of Subnode objects to filter.
+ * @param {string} node - The node to filter by.
+ * @return {Subnode[]} - A new list of Subnode objects with only those whose node property matches the given node.
+ */
+
 export const nodeFilter = (subnodes: Subnode[], node: string) => {
 	let subs = []
 	for (const sub of subnodes) {
-		if (sub.node == node) {
+		if (sub.node.toLowerCase() == node.toLowerCase()) {
 			subs.push(sub)
 		}
 	}
 	return subs
 }
 
-export const subnodes = (dirPath, arrayOfFiles: Subnode[], user): Subnode[] => {
+/**
+ * Recursively gets all subnodes of a directory path that are markdown files.
+ * 
+ * @param dirPath - The path to the directory to get subnodes from.
+ * @param arrayOfFiles - An array of subnodes to add to. Defaults to an empty array.
+ * @param user - The user getting the subnodes.
+ * @returns An array of subnodes in the given directory path.
+ */
+
+export const subnodes = (dirPath: string, arrayOfFiles: Subnode[], user: string): Subnode[] => {
 	const files = fs.readdirSync(dirPath)
 
 	arrayOfFiles = arrayOfFiles || []
